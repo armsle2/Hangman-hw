f57bc739e9c118310fa996d4399443624b959fee
Leaveil Armstrong <FlyRebel@server.theflyrebel.com>
2017-11-19 20:01:54 -0500

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+var hitch = ["h", "i", "t", "c", "h"]
+var batman = ["b", "a", "t", "m", "a", "n"]
+var pif = ["p", "a", "i", "d", "i", "n", "f", "u", "l", "l"]
+var collateral = ["c", "o,", "l", "l", "a", "t", "e", "r", "a", "l"]
+var guessesLeft = 7
+var wins = 0
+var losses = 0
+var wordLines = " - "
+// var userGuess = function(event){
+// 			$(document).on("keyup", event.key)}
+// console.log(userGuess)
+
+
+
+
+$(document).ready(function(){
+
+
+//create word bank
+
+//load first word after user presses any key
+$(document).keyup(function(event){
+	var userGuess = event.key 
+	console.log($("#game-word")[0].innerHTML)
+	if ($("#game-word")[0].innerHTML === ""){
+		hitchArray();
+
+	}else {console.log(userGuess)}
+	
+
+});
+
+
+//have word lines loaded that match the number of letters for word being guessed
+function hitchArray(){
+	for (var i = 0; i < hitch.length; i++){
+		console.log(hitch[i]);
+		var gameWordDiv = $("#game-word");
+		var wordLetters = wordLines[i];
+		$(gameWordDiv).append(wordLines);
+		
+	}
+}
+
+// hitchArray();
+	
+
+//receive user guess input from keyboard
+	//if user guess is correct 
+		//display letter in place of appropriate line for word
+	//if user guess is wrong 
+		//add the incorrect letter under the "letters guessed"
+		//decrease guesses left by 1
+	//if guesses left equals 0
+		//display alert that says you lost
+		//increase losses by 1
+		//load new word in column 1
+
+});
